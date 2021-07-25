bc934a856014484b1180196233f202be825a02d0
kahirokunn <okinakahiro@gmail.com>
2021-07-26 00:02:05 +0900

    support: hard link

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use fuse::{
     FileAttr, FileType, Filesystem, ReplyAttr, ReplyCreate, ReplyData, ReplyDirectory, ReplyEntry,
     ReplyWrite, Request,
 };
-use libc::{EACCES, ENOENT};
+use libc::{EACCES, EEXIST, ENOENT};
 use std::env;
 use std::ffi::OsStr;
 use std::{collections::HashMap, str::FromStr};
@@ -17,9 +17,13 @@ const TTL: Timespec = Timespec { sec: 1, nsec: 0 };
 
 type INode = u64;
 
-struct File {
+struct HardLink {
     parent_ino: INode,
     name: String,
+}
+
+struct File {
+    hard_links: Vec<HardLink>,
     attr: FileAttr,
     generation: u64,
 }
@@ -55,15 +59,6 @@ fn new_file_attr(ino: INode, size: u64, ftype: FileType, uid: u32, gid: u32) -> 
     }
 }
 
-fn find_child_file(m: &MemFS, parent_ino: INode, name: &str) -> Option<INode> {
-    for (ino, f) in m.inodes.iter() {
-        if f.parent_ino == parent_ino && f.name == name {
-            return Some(ino.clone());
-        }
-    }
-    None
-}
-
 impl Filesystem for MemFS {
     fn getattr(&mut self, _req: &Request, ino: INode, reply: ReplyAttr) {
         for (&inode, f) in self.inodes.iter() {
@@ -92,19 +87,24 @@ impl Filesystem for MemFS {
         reply.add(2, 1, FileType::Directory, "..");
         let mut reply_add_offset = 2;
         for (_, f) in self.inodes.iter() {
-            if ino == f.parent_ino {
-                let attr = f.attr;
-                let name = &f.name;
-                reply.add(attr.ino, reply_add_offset, attr.kind, name);
-                reply_add_offset += 1;
+            for h in f.hard_links.iter() {
+                if h.parent_ino == ino {
+                    reply.add(f.attr.ino, reply_add_offset, f.attr.kind, h.name.clone());
+                    reply_add_offset += 1;
+                }
             }
         }
         reply.ok();
     }
 
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
+        let n = name.to_str().unwrap();
         for (_, f) in self.inodes.iter() {
-            if f.parent_ino == parent && name.to_str().unwrap() == f.name.as_str() {
+            if let Some(_) = f
+                .hard_links
+                .iter()
+                .position(|h| h.parent_ino == parent && h.name == n)
+            {
                 reply.entry(&TTL, &f.attr, f.generation);
                 return;
             }
@@ -126,8 +126,10 @@ impl Filesystem for MemFS {
         self.inodes.insert(
             inode,
             File {
-                parent_ino: parent,
-                name: name.to_str().unwrap().to_string(),
+                hard_links: vec![HardLink {
+                    parent_ino: parent,
+                    name: name.to_str().unwrap().to_string(),
+                }],
                 attr: f,
                 generation: 0,
             },
@@ -148,8 +150,10 @@ impl Filesystem for MemFS {
         self.inodes.insert(
             inode,
             File {
-                parent_ino: _parent,
-                name: _name.to_str().unwrap().to_string(),
+                hard_links: vec![HardLink {
+                    parent_ino: _parent,
+                    name: _name.to_str().unwrap().to_string(),
+                }],
                 attr: f,
                 generation: 0,
             },
@@ -202,20 +206,8 @@ impl Filesystem for MemFS {
         let x = String::from_utf8(data.to_vec()).expect("fail to-string");
         self.datas.insert(ino, x);
         if let Some(f) = self.inodes.get_mut(&ino) {
-            let parent_ino = f.parent_ino;
-            let name = f.name.clone();
-            *f = File {
-                parent_ino,
-                name,
-                attr: new_file_attr(
-                    ino,
-                    length as u64,
-                    FileType::RegularFile,
-                    _req.uid(),
-                    _req.gid(),
-                ),
-                generation: f.generation + 1,
-            }
+            f.attr.size = length as u64;
+            f.generation += 1;
         }
         reply.written(length as u32);
     }
@@ -249,14 +241,33 @@ impl Filesystem for MemFS {
     }
 
     fn unlink(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: fuse::ReplyEmpty) {
-        let maybe_ino = find_child_file(self, _parent, _name.to_str().unwrap());
-        match maybe_ino {
-            Some(ino) => {
-                self.inodes.remove(&ino);
-                self.datas.remove(&ino);
-                reply.ok();
+        let mut ok_reply = false;
+        let mut delete_ino: u64 = 0;
+        let name = _name.to_str().unwrap();
+        for (ino, f) in self.inodes.iter_mut() {
+            if let Some(_) = f
+                .hard_links
+                .iter()
+                .position(|h| h.parent_ino == _parent && h.name == name)
+            {
+                let index = f.hard_links.iter().position(|x| x.name == name).unwrap();
+                f.hard_links.remove(index);
+                f.attr.nlink -= 1;
+                if f.hard_links.len() == 0 {
+                    delete_ino = ino.clone();
+                }
+                ok_reply = true;
             }
-            None => reply.error(EACCES),
+        }
+        if delete_ino != 0 {
+            self.inodes.remove(&delete_ino);
+            self.datas.remove(&delete_ino);
+        }
+
+        if ok_reply {
+            reply.ok();
+        } else {
+            reply.error(EACCES);
         }
     }
 
@@ -277,8 +288,10 @@ impl Filesystem for MemFS {
         self.inodes.insert(
             inode,
             File {
-                parent_ino: _parent,
-                name: _name.to_str().unwrap().to_string(),
+                hard_links: vec![HardLink {
+                    parent_ino: _parent,
+                    name: _name.to_str().unwrap().to_string(),
+                }],
                 attr: f,
                 generation: 0,
             },
@@ -310,6 +323,36 @@ impl Filesystem for MemFS {
             }
         }
     }
+
+    fn link(
+        &mut self,
+        _req: &Request,
+        _ino: u64,
+        _newparent: u64,
+        _newname: &OsStr,
+        reply: ReplyEntry,
+    ) {
+        let name = _newname.to_str().unwrap().to_string();
+
+        match self.inodes.get_mut(&_ino) {
+            Some(f) => match f
+                .hard_links
+                .iter()
+                .position(|h| h.parent_ino == _newparent && h.name == name)
+            {
+                Some(_) => reply.error(EEXIST),
+                None => {
+                    f.attr.nlink += 1;
+                    f.hard_links.push(HardLink {
+                        parent_ino: _newparent,
+                        name: name,
+                    });
+                    reply.entry(&TTL, &f.attr, f.generation);
+                }
+            },
+            None => reply.error(ENOENT),
+        }
+    }
 }
 
 fn main() {
@@ -321,8 +364,10 @@ fn main() {
     inodes.insert(
         1,
         File {
-            parent_ino: 0,
-            name: "/".to_string(),
+            hard_links: vec![HardLink {
+                parent_ino: 0,
+                name: "/".to_string(),
+            }],
             attr: new_file_attr(1, 0, FileType::Directory, 501, 20),
             generation: 0,
         },
